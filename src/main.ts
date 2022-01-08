d713c247021f2242828f140409aa2e6caa541f59
ivankl <iwan.kaleiczyk@proexe.eu>
2022-01-08 22:43:05 +0100

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './app/App.vue'
+import router from './app/router'
+import store from './app/store'
+import vuetify from './app/plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
